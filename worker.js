3e2b3afa5d7f98a1901b001188263a60b698f076
Thomas Fritzsche <tf@noto.de>
2025-10-15 18:20:47 +0200

    Cleanup

--- worker.js
+++ worker.js
@@ -1,21 +1,19 @@
 import createModule from './pdftex.js'
-import { ROOT_HASH  } from './objects/root.js'
-import { IndexDBCache  } from './cache.js'
+import { ROOT_HASH } from './objects/root.js'
+import { IndexDBCache } from './cache.js'
 
-const FileObjects = {}
+const TeXLiveFileObjects = {}
 
-// Das Konfigurationsobjekt für das Emscripten-Modul
+// Configuration of the emscripten module
 const Module = {
     thisProgram: '/pdflatex',
-    //thisProgram: '/pdftex',    
-
-  'print': (text) => { 
-      postMessage({
+    'print': (text) => {
+        postMessage({
             command: 'print',
             message: text
         })
-   },
-  'printErr': (text) => { console.error('stderr: ' + text) },
+    },
+    'printErr': (text) => { console.error('stderr: ' + text) },
 
     preRun: [async () => {
         console.log("Initialize the filesystem...")
@@ -23,14 +21,14 @@ const Module = {
 
         const TEXFS = Module.TEXFS
 
-      //  await IndexDBCache.loadFileObjectsFromIndexedDB(FileObjects)
+        //  await IndexDBCache.loadFileObjectsFromIndexedDB(FileObjects)
 
         FS.chdir('/')
-//        FS.mkdir("/working")
-//        FS.chdir("/working")
-//        FS.createDataFile("/working", 'example.tex', example_tex, true, true)   
+        //        FS.mkdir("/working")
+        //        FS.chdir("/working")
+        //        FS.createDataFile("/working", 'example.tex', example_tex, true, true)   
 
-     //   FS.createLazyFile("/texlive", "texmf.cnf", "texlive/texmf.cnf", true, false)
+        //   FS.createLazyFile("/texlive", "texmf.cnf", "texlive/texmf.cnf", true, false)
         FS.createDataFile("/", Module.thisProgram, "dummy for kpathsea", true, true)
 
         //console.log(TeXLive)
@@ -38,18 +36,17 @@ const Module = {
         //const root_fs_hash = '032e36abb8411cdac23eb83318c475ce380aee4e'
         const root_fs_hash = ROOT_HASH
         //const root_fs_hash = 'f3c67207b857afff679a7ba18ca654e8f0e0172c'
-        
+
         Module.FS.mkdir('/texlive')
-        Module.FS.mount(TEXFS,  { 
-        rootHash: root_fs_hash, 
-        debug: false, 
-        objects: FileObjects }, '/texlive')
+        Module.FS.mount(TEXFS, {
+            rootHash: root_fs_hash,
+            debug: false,
+            objects: TeXLiveFileObjects
+        }, '/texlive')
 
         FS.mkdir("/working")
         FS.chdir("/working")
-     //   FS.createDataFile("/working", 'example.tex', example_tex, true, true)   
-
-
+        // Set enviromengt variable to make TeX happy find all the path in /texlive
         const ENV = Module.ENV
         ENV.TEXMFCNF = '/texlive/:/texlive/texmf-dist/web2c/'
         ENV.TEXMFROOT = '/texlive'
@@ -58,75 +55,60 @@ const Module = {
         ENV.TEXMFSYSVAR = '/texlive/texmf-var'
         ENV.TEXMFSYSCONFIG = '/texlive/texmf-config'
         ENV.TEXMFVAR = '/texlive/texmf-var'
-        
         console.log("preRun: Setup Ready.")
-
-
-
-    }], 
+    }],
 }
 
-await IndexDBCache.loadFileObjectsFromIndexedDB(FileObjects)
-// 2. Die Factory-Funktion aufrufen und das Promise verarbeiten
+console.log("Start restoring all the TeX Live VFS objects")
+await IndexDBCache.loadFileObjectsFromIndexedDB(TeXLiveFileObjects)
+console.log("Restore all the files from IndexDB finished.")
+// Call the modules Factors Class and process the Promise
 createModule(Module).then((instance) => {
     console.log("Modul-Instanz ready.")
-     const FS = instance.FS
-   
-     const args = [  'example.tex'];
-//     const args = ['pdftex', '-ini','-jobname=latex','-progname=latex','-translate-file=cp227.tcx','*latex.ini'];
- //    MyModule.callMain(args);
-   //  sendToMain('example.pdf')
-
+    const FS = instance.FS
+    const args = ['example.tex']
 })
 
-self.onmessage = function(event) {
-    const { command, texContent, fileName } = event.data;
-
+self.onmessage = function (event) {
+    const { command, texContent, fileName } = event.data
     if (command === 'compile') {
-        const pdfFileName = fileName.replace('.tex', '.pdf'); 
-    //    const workingDir = '/working'; // Das Verzeichnis, in dem pdflatex arbeitet
-        
+        const pdfFileName = fileName.replace('.tex', '.pdf')
         try {
-
-            Module.FS.writeFile(fileName, texContent, { encoding: 'utf8' });
-
-            console.log(`TeX-Datei '${fileName}' im VFS gespeichert. Starte pdflatex...`);
-            const args = [fileName];
-            Module.callMain(args);
-            IndexDBCache.storeFileObjectsInIndexedDB(FileObjects)
-            sendToMain(pdfFileName)            
+            Module.FS.writeFile(fileName, texContent, { encoding: 'utf8' })
+            console.log(`TeX-File '${fileName}' in VFS saved. Starting pdflatex...`)
+            const args = [fileName]
+            Module.callMain(args)
+            IndexDBCache.storeFileObjectsInIndexedDB(TeXLiveFileObjects)
+            sendToMain(pdfFileName)
         } catch (e) {
-            console.error('Fehler während der pdflatex-Ausführung im Worker:', e);
+            console.error('Error durign execution in worker', e)
             postMessage({
                 command: 'error',
-                message: `Fehler beim Erstellen der PDF: ${e.message}`
-            });
+                message: `Error during pdf processing in worker: ${e.message}`
+            })
         } finally {
-            // Optional: Aufräumarbeiten im VFS
+            // Optional: Clean VFS
             // Module.FS.unlink(fileName); 
         }
     }
-};
+}
 
-// Behält die Definition der Sende-Funktion bei
-function sendToMain(fileName) {
-    // Nimmt jetzt den Dateinamen als Argument, da er variabel sein könnte
-    
+
+const sendToMain = (fileName) => {
     try {
-        const fileContent = Module.FS.readFile(fileName, { encoding: 'binary' });
-
+        const fileContent = Module.FS.readFile(fileName, { encoding: 'binary' })
         postMessage({
             command: 'pdfReady',
-            data: fileContent.buffer, 
+            data: fileContent.buffer,
             fileName: fileName
-        }, [fileContent.buffer]); 
+        }, [fileContent.buffer])
 
     } catch (e) {
-        console.error('Error sending PDF file', e);
+        console.error('Error sending PDF file', e)
         postMessage({
             command: 'error',
             message: 'Error while sending PDF to the main thread.'
-        });
+        })
     }
 }
 
